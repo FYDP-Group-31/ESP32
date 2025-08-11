bac0790bbb6eb26e070d961df6a2f4ebc411010c
shong0504 <seungminhong2002@gmail.com>
2025-08-11 00:31:03 -0700

    Disabled minimal build, enabled PSRAM

--- p4/main/main.c
+++ p4/main/main.c
@@ -6,9 +6,16 @@
 
 void app_main(void)
 {
-    void *buf = heap_caps_malloc(2 * 1024 * 1024, MALLOC_CAP_SPIRAM);
-    ESP_LOGI("PSRAM", "Buffer = %p", buf);
-    assert(buf != NULL);
+    void *buf = heap_caps_malloc(1024, MALLOC_CAP_DMA);
+    void *buf2 = heap_caps_malloc(1024, MALLOC_CAP_SPIRAM);
+    void *buf3 = heap_caps_malloc(1024, MALLOC_CAP_SIMD);
+    ESP_LOGI("DMA", "Buffer = %p", buf);
+    ESP_LOGI("SPIRAM", "Buffer = %p", buf2);
+    ESP_LOGI("SIMD", "Buffer = %p", buf3);
+    heap_caps_print_heap_info(MALLOC_CAP_INTERNAL);
+    heap_caps_print_heap_info(MALLOC_CAP_DMA);
+    heap_caps_print_heap_info(MALLOC_CAP_SPIRAM);
+    
     for (;;)
     {
         vTaskDelay(pdMS_TO_TICKS(1));
